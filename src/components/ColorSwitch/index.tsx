3fc333fd9b1e192e2bb0df2da3e3376298afb276
kritical0613 <kulishan0613@gmail.com>
2022-06-02 15:19:55 -0500

    fix: update switch theme

--- src/components/ColorSwitch/index.tsx
+++ src/components/ColorSwitch/index.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import Image from 'next/image'
 
 const ColorSwith = (): JSX.Element => {
+
+  const [isDark, setDark] = useState(false);
 
   useEffect(() => {
     if (
@@ -18,30 +20,25 @@ const ColorSwith = (): JSX.Element => {
     document.documentElement.classList.remove('dark')
     document.documentElement.classList.remove('light')
   }
+
   const handleDarkTheme = () => {
     handleClearTheme()
     document.documentElement.classList.add('dark')
     localStorage.setItem('color-theme', 'dark')
+    setDark(true)
   }
+
   const handleLightTheme = () => {
     handleClearTheme()
     document.documentElement.classList.add('light')
     localStorage.setItem('color-theme', 'light')
+    setDark(false)
   }
 
   return (
-    <div className="flex  bg-light-bg dark:bg-dark-bg rounded-md transition-colors">
-      <div
-        className="items-center justify-center cursor-pointer rounded-lg shadow px-3 h-9 bg-gray-50 dark:bg-gray-800 text-gray-800 dark:text-gray-100 flex dark:hidden transition-all"
-        onClick={handleDarkTheme}
-      >
-        <Image src="/sun.svg" alt="sun" width={25} height={24} />
-      </div>
-      <div
-        className="items-center justify-center cursor-pointer rounded-lg shadow px-3 h-9 bg-gray-50 dark:bg-gray-800 text-gray-800 dark:text-gray-100 hidden dark:flex transition-all"
-        onClick={handleLightTheme}
-      >
-        <Image src="/moon.svg" alt="moon" width={25} height={24} />
+    <div className="flex bg-light-bg dark:bg-dark-bg rounded-md transition-colors">
+      <div className="items-center justify-center cursor-pointer rounded-lg shadow-md px-3 h-9 bg-gray-50 dark:bg-gray-800 text-gray-800 dark:text-gray-100 flex transition-all" onClick={!isDark ? handleDarkTheme : handleLightTheme}>
+        <Image src={`/${!isDark ? 'sun' : 'moon'}.svg`} alt="sun" width={25} height={24} />
       </div>
     </div>
   )
